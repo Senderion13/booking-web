d80d0dd4318141ce562abbd5c5d6c113c12d6292
kartonqa <117667542+kartonqa@users.noreply.github.com>
2024-05-22 17:42:39 +0300

    Reapply "Dynamic routes"
    This reverts commit 1109fbccccf80ae21d9479d5851d397edb1fb595.

---
+++ src/app/hotels/[hotelId]/rooms/page.tsx
@@ -0,0 +1,10 @@
+export default function RoomList() {
+  return (
+    <>
+      <h1>Room List</h1>
+      <h2>Room 1</h2>
+      <h2>Room 2</h2>
+      <h2>Room 3</h2>
+    </>
+  );
+}
---
+++ src/app/hotels/page.tsx
@@ -0,0 +1,10 @@
+export default function HotelList() {
+  return (
+    <>
+      <h1>Hotel List</h1>
+      <h2>Hotel 1</h2>
+      <h2>Hotel 2</h2>
+      <h2>Hotel 3</h2>
+    </>
+  );
+}
