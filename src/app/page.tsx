13c412b620e8dc2d8de7138647afd7bd6231053b
kartonqa <117667542+kartonqa@users.noreply.github.com>
2024-06-01 20:31:09 +0300

    footer/stars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Head from "../components/head";
 import Search from "../components/search";
 import Hotels from "../components/hotels";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start pb-[9.875rem] box-border gap-[2.375rem] mq750:gap-[1.188rem]">
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start gap-[2.375rem] leading-[normal] tracking-[normal] mq750:gap-[1.188rem]">
       <Head />
       <section className="self-stretch flex flex-row items-start justify-center py-[0rem] px-[1.25rem] box-border max-w-full">
         <div className="w-[75rem] flex flex-col items-start justify-start gap-[4rem] max-w-full lg:gap-[2rem] mq750:gap-[1rem]">
@@ -12,6 +13,7 @@ export default function Home() {
           <Hotels />
         </div>
       </section>
+      <Footer />
     </div>
   );
 }
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,16 +1,12 @@
-import type { NextPage } from "next";
 import Image from "next/image";
+import Rating from "@mui/material/Rating";
 
-export type SearchType = {
-  className?: string;
-};
-
-const Search: NextPage<SearchType> = ({ className = "" }) => {
+export default function Search() {
   return (
     <div
-      className={`self-stretch rounded bg-seashell flex flex-row items-start justify-center py-[1rem] px-[1.5rem] box-border gap-[1.5rem] max-w-full mq1050:flex-wrap ${className}`}
+      className={`self-stretch rounded bg-seashell flex flex-row items-start justify-center py-[1rem] px-[1.5rem] box-border gap-[1.5rem] max-w-full mq1050:flex-wrap`}
     >
-      <div className="flex-1 rounded bg-white flex flex-row items-start justify-start py-[0.875rem] px-[1rem] box-border gap-[0.5rem] min-w-[10.875rem] max-w-full mq450:flex-wrap">
+      <div className="flex-1 rounded bg-white flex flex-row items-start justify-start p-[1rem] box-border gap-[0.5rem] min-w-[10.875rem] max-w-full mq450:flex-wrap">
         <Image
           className="h-[1.875rem] w-[1.875rem] relative rounded overflow-hidden shrink-0"
           alt=""
@@ -18,15 +14,15 @@ const Search: NextPage<SearchType> = ({ className = "" }) => {
           width={30}
           height={30}
         />
-        <div className="w-[9rem] flex flex-col items-start justify-start pt-[0.25rem] px-[0rem] pb-[0rem] box-border">
+        <div className="w-[9rem] flex flex-col items-start justify-start pt-[0.25rem] box-border">
           <input
-            className="[border:none] [outline:none] font-namu text-[1.125rem] bg-[transparent] self-stretch h-[1.375rem] relative text-black text-left inline-block min-w-[5.375rem] p-0"
+            className="[border:none] [outline:none] text-[1.125rem] bg-[transparent] self-stretch h-[1.375rem] relative text-black text-left inline-block min-w-[5.375rem] p-0"
             placeholder="Оберіть місто"
             type="text"
           />
         </div>
       </div>
-      <div className="flex-1 rounded bg-white flex flex-row items-start justify-start py-[0.875rem] px-[1rem] box-border gap-[0.5rem] min-w-[10.875rem] max-w-full mq450:flex-wrap">
+      <div className="flex-1 rounded bg-white flex flex-row items-start justify-start p-[1rem] box-border gap-[0.5rem] min-w-[10.875rem] max-w-full mq450:flex-wrap">
         <Image
           className="h-[1.875rem] w-[1.875rem] relative rounded overflow-hidden shrink-0"
           loading="lazy"
@@ -35,16 +31,33 @@ const Search: NextPage<SearchType> = ({ className = "" }) => {
           width={30}
           height={30}
         />
-        <div className="w-[9rem] flex flex-col items-start justify-start pt-[0.25rem] px-[0rem] pb-[0rem] box-border">
+        <div className="w-[9rem] flex flex-col items-start justify-start pt-[0.25rem] box-border min-w-[10rem]">
           <input
-            className="[border:none] [outline:none] font-namu text-[1.125rem] bg-[transparent] self-stretch h-[1.375rem] relative text-black text-left inline-block min-w-[5.375rem] p-0"
+            className="[border:none] [outline:none] text-[1.125rem] bg-[transparent] self-stretch h-[1.375rem] relative text-black text-left inline-block min-w-[5.375rem] p-0"
             placeholder="Кількість людей"
-            type="text"
+            type="number"
           />
         </div>
       </div>
-      <div className="flex-[0.6575] bg-white flex flex-row items-start justify-center py-[1.062rem] px-[4.125rem] box-border gap-[1.125rem] min-w-[10.875rem] max-w-full mq450:flex-wrap mq450:pl-[1.25rem] mq450:pr-[1.25rem] mq450:box-border mq450:flex-1">
-        <Image
+      <div className="flex-[0.6575] bg-white flex flex-row items-start justify-center py-[1rem] px-[4.125rem] box-border gap-[1.125rem] min-w-[10.875rem] max-w-full mq450:flex-wrap mq450:px-[1.25rem] mq450:box-border mq450:flex-1">
+        <Rating
+          name="half-rating"
+          defaultValue={2.5}
+          precision={0.5}
+          size="large"
+        />
+      </div>
+      <button className="cursor-pointer [border:none] py-[1.125rem] px-[1.437rem] bg-rosybrown-100 rounded flex flex-row items-start justify-start hover:bg-rosybrown-200">
+        <div className="relative text-[1.125rem]  text-white text-left inline-block min-w-[3.875rem]">
+          Пошук
+        </div>
+      </button>
+    </div>
+  );
+}
+
+/*
+<Image
           className="h-[1.5rem] w-[1.5rem] relative overflow-hidden shrink-0 min-h-[1.5rem]"
           loading="lazy"
           alt=""
@@ -84,14 +97,4 @@ const Search: NextPage<SearchType> = ({ className = "" }) => {
           width={24}
           height={24}
         />
-      </div>
-      <button className="cursor-pointer [border:none] py-[1.125rem] px-[1.437rem] bg-rosybrown-100 rounded flex flex-row items-start justify-start hover:bg-rosybrown-200">
-        <div className="relative text-[1.125rem] font-namu text-white text-left inline-block min-w-[3.875rem]">
-          Пошук
-        </div>
-      </button>
-    </div>
-  );
-};
-
-export default Search;
+*/
