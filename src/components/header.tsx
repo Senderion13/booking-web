337c02f8b87db0f873aaaa88a0e5f27a13d433ed
Andrii Sierov <serovandrey2004@gmail.com>
2024-06-02 10:37:10 +0300

    changed href for our team

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,13 +4,16 @@ export default function Header() {
   return (
     <div className="px-6 self-stretch flex flex-row items-start justify-between text-center text-[1.5rem] text-white font-namu mq450:flex-wrap">
       <div className="flex flex-row items-start justify-start p-[0.625rem]">
-        <a className="cursor-pointer [text-decoration:none] relative text-[inherit]">
+        <a
+          href="/"
+          className="cursor-pointer [text-decoration:none] relative text-[inherit]"
+        >
           Booking.UA
         </a>
       </div>
       <div className="cursor-pointer flex flex-col items-start justify-start pt-[0.156rem] px-[0rem] pb-[0rem] text-[1rem]">
         <a
-          href="about-us"
+          href="our-team"
           className="flex flex-row items-start justify-start p-[0.35rem] gap-[0.1rem]"
         >
           <Image
@@ -21,7 +24,7 @@ export default function Header() {
             width={24}
             height={24}
           />
-          <div className="flex flex-col items-center justify-center pt-[0.156rem] hover:font-bold">
+          <div className="flex flex-col items-center justify-center pt-[0.156rem]">
             <p className="[text-decoration:none] relative text-[inherit] inline-block min-w-[7.5rem]">
               Наша команда
             </p>
