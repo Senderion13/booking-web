a7a50ca2559b0a5919e097b65f47b43bdc28aa3e
Sierov Andrii <serovandrey2004@gmail.com>
2024-06-05 12:18:46 +0300

    changed title in hotels component

--- src/components/hotels.tsx
+++ src/components/hotels.tsx
@@ -20,7 +20,7 @@ const Hotels: NextPage = () => {
       className={`self-stretch flex flex-col items-start justify-start gap-[2.25rem] max-w-full text-left text-[2.25rem] text-gray font-namu mq750:gap-[1.125rem]`}
     >
       <h3 className="m-0 relative text-inherit font-normal font-inherit inline-block max-w-full mq450:text-[1.375rem] mq1050:text-[1.813rem]">
-        Найкращі готелі Києва
+        Найкращі готелі Світу
       </h3>
       <div className="self-stretch overflow-x-auto grid flex-row items-start justify-start gap-[1.312rem] max-w-full grid-cols-[repeat(3,_minmax(297px,_1fr))] mq750:grid-cols-[minmax(297px,_1fr)] mq1050:justify-center mq1050:grid-cols-[repeat(2,_minmax(297px,_515px))]">
         {!hotels
