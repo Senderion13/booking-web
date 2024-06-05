06221468e40e5cf1cc4d5a4ec071567f6f25e9eb
Sierov Andrii <serovandrey2004@gmail.com>
2024-06-05 09:15:27 +0300

    resolved dependencies

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -9,7 +9,7 @@ import Button from "@mui/material/Button";
 
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import SwipeableViews from "react-swipeable-views";
+import SwipeableViews from "react-swipeable-views-react-18-fix";
 import { autoPlay } from "react-swipeable-views-utils";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
